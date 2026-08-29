64b961eceda40a349639e5987138a59197706eab
Konstantin S. <havendv@gmail.com>
2026-08-29 08:59:45 +0400

    feat: Updated OpenAPI spec (#5)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Spotify/Generated/Spotify.JsonConverters.CursorPagingSimplifiedArtistObject.g.cs
+++ src/libs/Spotify/Generated/Spotify.JsonConverters.CursorPagingSimplifiedArtistObject.g.cs
@@ -17,99 +17,34 @@ namespace Spotify.JsonConverters
 
             using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
             var __rawJson = __jsonDocument.RootElement.GetRawText();
-            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
-            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-            {
-                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
-                {
-                    __jsonProps.Add(__jsonProp.Name);
-
-                }
-            }
-
-            var __score0 = 0;
-            if (__jsonProps.Contains("cursors")) __score0++;
-            if (__jsonProps.Contains("href")) __score0++;
-            if (__jsonProps.Contains("limit")) __score0++;
-            if (__jsonProps.Contains("next")) __score0++;
-            if (__jsonProps.Contains("total")) __score0++;
-            var __score1 = 0;
-            if (__jsonProps.Contains("items")) __score1++;
-            var __bestScore = 0;
-            var __bestIndex = -1;
-            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
-            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
 
             global::Spotify.CursorPagingObject? cursorPagingObject = default;
+            try
+            {
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingObject> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingObject).Name}");
+                cursorPagingObject = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+            }
+            catch (global::System.Text.Json.JsonException)
+            {
+            }
+            catch (global::System.InvalidOperationException)
+            {
+            }
+
             global::Spotify.CursorPagingSimplifiedArtistObjectVariant2? cursorPagingSimplifiedArtistObjectVariant2 = default;
-            if (__bestIndex >= 0)
+            try
             {
-                if (__bestIndex == 0)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingObject> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingObject).Name}");
-                        cursorPagingObject = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
-                else if (__bestIndex == 1)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingSimplifiedArtistObjectVariant2> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2).Name}");
-                        cursorPagingSimplifiedArtistObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingSimplifiedArtistObjectVariant2> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2).Name}");
+                cursorPagingSimplifiedArtistObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
             }
-
-            if (cursorPagingObject == null && cursorPagingSimplifiedArtistObjectVariant2 == null)
+            catch (global::System.Text.Json.JsonException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingObject> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingObject).Name}");
-                    cursorPagingObject = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
-            if (cursorPagingObject == null && cursorPagingSimplifiedArtistObjectVariant2 == null)
+            catch (global::System.InvalidOperationException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingSimplifiedArtistObjectVariant2> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2).Name}");
-                    cursorPagingSimplifiedArtistObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
             var __value = new global::Spotify.CursorPagingSimplifiedArtistObject(
                 cursorPagingObject,
 
@@ -128,18 +63,46 @@ namespace Spotify.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
+
+            writer.WriteStartObject();
+            var __writtenPropertyNames = new global::System.Collections.Generic.HashSet<string>(global::System.StringComparer.Ordinal);
             if (value.IsCursorPagingObject)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingObject?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingObject).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.CursorPagingObject!, typeInfo);
+                var __element0 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.CursorPagingObject!, typeInfo);
+                if (__element0.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element0.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
-            else if (value.IsCursorPagingSimplifiedArtistObjectVariant2)
+            if (value.IsCursorPagingSimplifiedArtistObjectVariant2)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.CursorPagingSimplifiedArtistObjectVariant2?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.CursorPagingSimplifiedArtistObjectVariant2).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.CursorPagingSimplifiedArtistObjectVariant2!, typeInfo);
+                var __element1 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.CursorPagingSimplifiedArtistObjectVariant2!, typeInfo);
+                if (__element1.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element1.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
+            writer.WriteEndObject();
         }
     }
 }
--- src/libs/Spotify/Generated/Spotify.JsonConverters.PlaylistOwnerObject.g.cs
+++ src/libs/Spotify/Generated/Spotify.JsonConverters.PlaylistOwnerObject.g.cs
@@ -17,99 +17,34 @@ namespace Spotify.JsonConverters
 
             using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
             var __rawJson = __jsonDocument.RootElement.GetRawText();
-            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
-            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-            {
-                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
-                {
-                    __jsonProps.Add(__jsonProp.Name);
-
-                }
-            }
-
-            var __score0 = 0;
-            if (__jsonProps.Contains("external_urls")) __score0++;
-            if (__jsonProps.Contains("href")) __score0++;
-            if (__jsonProps.Contains("id")) __score0++;
-            if (__jsonProps.Contains("type")) __score0++;
-            if (__jsonProps.Contains("uri")) __score0++;
-            var __score1 = 0;
-            if (__jsonProps.Contains("display_name")) __score1++;
-            var __bestScore = 0;
-            var __bestIndex = -1;
-            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
-            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
 
             global::Spotify.PlaylistUserObject? user = default;
+            try
+            {
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistUserObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistUserObject> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistUserObject).Name}");
+                user = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+            }
+            catch (global::System.Text.Json.JsonException)
+            {
+            }
+            catch (global::System.InvalidOperationException)
+            {
+            }
+
             global::Spotify.PlaylistOwnerObjectVariant2? playlistOwnerObjectVariant2 = default;
-            if (__bestIndex >= 0)
+            try
             {
-                if (__bestIndex == 0)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistUserObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistUserObject> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistUserObject).Name}");
-                        user = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
-                else if (__bestIndex == 1)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistOwnerObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistOwnerObjectVariant2> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistOwnerObjectVariant2).Name}");
-                        playlistOwnerObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistOwnerObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistOwnerObjectVariant2> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistOwnerObjectVariant2).Name}");
+                playlistOwnerObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
             }
-
-            if (user == null && playlistOwnerObjectVariant2 == null)
+            catch (global::System.Text.Json.JsonException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistUserObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistUserObject> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistUserObject).Name}");
-                    user = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
-            if (user == null && playlistOwnerObjectVariant2 == null)
+            catch (global::System.InvalidOperationException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistOwnerObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistOwnerObjectVariant2> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistOwnerObjectVariant2).Name}");
-                    playlistOwnerObjectVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
             var __value = new global::Spotify.PlaylistOwnerObject(
                 user,
 
@@ -128,18 +63,46 @@ namespace Spotify.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
+
+            writer.WriteStartObject();
+            var __writtenPropertyNames = new global::System.Collections.Generic.HashSet<string>(global::System.StringComparer.Ordinal);
             if (value.IsUser)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistUserObject), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistUserObject?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistUserObject).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.User!, typeInfo);
+                var __element0 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.User!, typeInfo);
+                if (__element0.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element0.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
-            else if (value.IsPlaylistOwnerObjectVariant2)
+            if (value.IsPlaylistOwnerObjectVariant2)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Spotify.PlaylistOwnerObjectVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Spotify.PlaylistOwnerObjectVariant2?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Spotify.PlaylistOwnerObjectVariant2).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.PlaylistOwnerObjectVariant2!, typeInfo);
+                var __element1 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.PlaylistOwnerObjectVariant2!, typeInfo);
+                if (__element1.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element1.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
+            writer.WriteEndObject();
         }
     }
 }
