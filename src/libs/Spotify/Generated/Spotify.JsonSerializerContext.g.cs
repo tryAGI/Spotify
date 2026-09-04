6f9a5aef767e25dfa5fcd7d6b386b90c1b6f4c25
Konstantin S. <havendv@gmail.com>
2026-09-04 06:17:06 +0400

    feat: Updated OpenAPI spec (#8)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Spotify/Generated/Spotify.JsonSerializerContext.g.cs
+++ src/libs/Spotify/Generated/Spotify.JsonSerializerContext.g.cs
@@ -692,6 +692,7 @@ namespace Spotify
     {
         private static readonly global::System.Text.Json.Serialization.Metadata.IJsonTypeInfoResolver Resolver = new LazyChunkResolver();
 
+
         private static readonly global::System.Text.Json.JsonSerializerOptions DefaultOptions = CreateDefaultOptions();
 
         /// <summary>
@@ -713,13 +714,8 @@ namespace Spotify
             return Resolver.GetTypeInfo(type, Options);
         }
 
-        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+         static void AddConverters(global::System.Text.Json.JsonSerializerOptions options)
         {
-            var options = new global::System.Text.Json.JsonSerializerOptions
-            {
-                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                TypeInfoResolver = Resolver,
-            };
             options.Converters.Add(new global::Spotify.JsonConverters.ItemJsonConverter());
             options.Converters.Add(new global::Spotify.JsonConverters.CurrentlyPlayingJsonConverter());
             options.Converters.Add(new global::Spotify.JsonConverters.QueueItemJsonConverter());
@@ -757,8 +753,17 @@ namespace Spotify
             options.Converters.Add(new global::Spotify.JsonConverters.AllOfJsonConverter<global::Spotify.PagingObject, global::Spotify.GetUsersTopArtistsAndTracksResponse2>());
             options.Converters.Add(new global::Spotify.JsonConverters.AllOfJsonConverter<global::Spotify.PagingObject, global::Spotify.GetCategoriesResponseCategories2>());
             options.Converters.Add(new global::Spotify.JsonConverters.UnixTimestampJsonConverter());
-
             options.Converters.Add(new LazyEnumJsonConverterFactory());
+        }
+
+        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+        {
+            var options = new global::System.Text.Json.JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                TypeInfoResolver = Resolver,
+            };
+            AddConverters(options);
 
             return options;
         }
